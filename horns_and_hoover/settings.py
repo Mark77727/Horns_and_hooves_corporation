8c12f0bd5d8002d3901128512abec79cff9e2418
Bolket <zozizga45@gmail.com>
2023-06-15 22:36:07 +0500

    Merge remote-tracking branch 'origin/horns_and_hooves' into horns_and_hooves

--- horns_and_hoover/settings.py
+++ horns_and_hoover/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,7 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "userup",
+    "userup.apps.UserupConfig",
+    'bootstrap5',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -55,7 +57,7 @@ ROOT_URLCONF = "horns_and_hoover.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ["templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -117,8 +119,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- userup/models.py
+++ userup/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+class portfolio(models.Model):
+    name = models.TextField(verbose_name="Название")
+    content = models.TextField(verbose_name="Описание")
+    cover = models.ImageField(upload_to="images/")
 
-# Create your models here.
