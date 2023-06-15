cd6a0fdcbf543484368bbef6ce345ff2207434fa
Bolket <zozizga45@gmail.com>
2023-06-15 22:56:58 +0500

    Merge remote-tracking branch 'origin/horns_and_hooves' into horns_and_hooves

--- userup/models.py
+++ userup/models.py
@@ -1,6 +1,11 @@
 from django.db import models
+
+
 class portfolio(models.Model):
     name = models.TextField(verbose_name="Название")
     content = models.TextField(verbose_name="Описание")
     cover = models.ImageField(upload_to="images/")
+    urls_path = models.URLField()
 
+    def __str__(self):
+        return self.name
